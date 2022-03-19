35b69d3390782f5b17d9f8a456e0f471357b9b0e
Joshan Rai <pradheon@gmail.com>
2022-03-19 12:09:31 -0700

    Initial Commit

---
+++ HabiTrack/HabiTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabiTrackApp.swift
+//  HabiTrack
+//
+//  Created by Joshan Rai on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HabiTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
